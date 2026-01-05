0a7c4215780cc71e32e5689bf2d6e479d3681018
abhijitkayal <kayalabhi04@gmail.com>
2026-01-05 15:20:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import React from "react";
 // import NotFound from "./pages/NotFound";
 
